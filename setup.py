9bef0d10e0bfa76ea9c5749e7c17de93fe59ecbb
cobets <cobets@gmail.com>
2023-11-22 16:14:49 +0200

    fix package setup

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, Extension
 dotspath_module = Extension('dotspath', sources=['./dotspath/module.cpp'])
 
 setup(
-    name='dotsgameenv',
-    version='0.1.2',
+    name='dots-game-env',
+    version='0.1.3',
     description='Python Environment for dots game',
     url='https://github.com/cobets/dots-env',
     author='Kobets Serhii',
     author_email='cobets@gmail.com',
     license='MIT',
+    packages=['dotsenv'],
     ext_modules=[dotspath_module],
     install_requires=[
         'numpy',
